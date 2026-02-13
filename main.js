24304d90c2bb5e316ceb9623ea0bc203f555e427
alliahbrown <formation@ubuntu.ubuntu-domain>
2026-02-13 16:58:58 +0100

    fin de journée

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
 "use strict";
-import { PerspectiveCamera, Scene, WebGLRenderer, AmbientLight, Clock, Color } from 'three';
+import { PerspectiveCamera, Scene, WebGLRenderer, Mesh, Vector2, AmbientLight, Clock, Color, Raycaster, MeshStandardMaterial } from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-// INSERT CODE HERE
+var raycaster = new Raycaster();
+var pointer = new Vector2();
 var scene = new Scene();
 var aspect = window.innerWidth / window.innerHeight;
 var camera = new PerspectiveCamera(75, aspect, 0.1, 1000);
-var light = new AmbientLight(0xffffff, 3.0); // soft white light
+var light = new AmbientLight(0xffffff, 3.0);
 scene.add(light);
 scene.background = new Color('lightgray');
 var renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 var controls = new OrbitControls(camera, renderer.domElement);
-controls.listenToKeyEvents(window); // optional
+controls.listenToKeyEvents(window);
 var plateau;
 var plateau_mesh;
+var INTERSECTED = null;
 var clock = new Clock();
 function loadData() {
     new GLTFLoader()
         .setPath('assets/models/')
-        .load('vinyl_player.glb', gltfReader);
+        .load('vinyl.glb', gltfReader);
 }
 function gltfReader(gltf) {
     var testModel = null;
@@ -28,25 +30,65 @@ function gltfReader(gltf) {
     if (testModel != null) {
         console.log("Model loaded: " + testModel);
         scene.add(gltf.scene);
-        plateau_mesh = scene.getObjectByName('pCylinder2_dit2_0');
+        plateau_mesh = scene.getObjectByName('platterMesh');
         var plateau_material = plateau_mesh.material;
-        plateau_material.wireframe = true;
+        // plateau_material.wireframe = true;
         plateau = plateau_mesh === null || plateau_mesh === void 0 ? void 0 : plateau_mesh.parent;
     }
     else {
-        console.log("Load FAILED.  ");
+        console.log("Load FAILED.");
     }
 }
 loadData();
 camera.position.z = 3;
+function onPointerMove(event) {
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
 // Main loop
 var animation = function () {
-    renderer.setAnimationLoop(animation); // requestAnimationFrame() replacement, compatible with XR 
-    // plateau?.rotateY(45);
+    renderer.setAnimationLoop(animation);
     renderer.render(scene, camera);
+    raycaster.setFromCamera(pointer, camera);
+    var intersects = raycaster.intersectObjects(scene.children, true);
+    if (intersects.length > 0) {
+        var found = false;
+        for (var i = 0; i < intersects.length; i++) {
+            if (intersects[i].object instanceof Mesh) {
+                var mesh = intersects[i].object;
+                if (mesh.material instanceof MeshStandardMaterial) {
+                    if (INTERSECTED != mesh) {
+                        if (INTERSECTED) {
+                            var oldMaterial = INTERSECTED.material;
+                            oldMaterial.emissive.setHex(INTERSECTED.userData.currentHex || 0x000000);
+                        }
+                        INTERSECTED = mesh;
+                        var material = INTERSECTED.material;
+                        INTERSECTED.userData.currentHex = material.emissive.getHex();
+                        material.emissive.setHex(0xff0000);
+                    }
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if (!found && INTERSECTED) {
+            var material = INTERSECTED.material;
+            material.emissive.setHex(INTERSECTED.userData.currentHex || 0x000000);
+            INTERSECTED = null;
+        }
+    }
+    else {
+        if (INTERSECTED) {
+            var material = INTERSECTED.material;
+            material.emissive.setHex(INTERSECTED.userData.currentHex || 0x000000);
+            INTERSECTED = null;
+        }
+    }
 };
 animation();
 window.addEventListener('resize', onWindowResize, false);
+window.addEventListener('pointermove', onPointerMove);
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
--- main.ts
+++ main.ts
@@ -1,104 +1,133 @@
 "use strict";
-
-import { Object3D } from 'node_modules/@types/three/build/three.cjs';
 import {
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
   BoxGeometry,
   Mesh,
+  Object3D,
+  Vector2,
   MeshNormalMaterial,
   AmbientLight,
   Clock,
   Color,
+  Raycaster,
   MeshStandardMaterial
 } from 'three';
-
-
 import {
   OrbitControls
 } from 'three/addons/controls/OrbitControls.js';
-
 import {
   GLTF,
   GLTFLoader
 } from 'three/addons/loaders/GLTFLoader.js';
 
-
-
-// INSERT CODE HERE
-
+const raycaster = new Raycaster();
+const pointer = new Vector2();
 const scene = new Scene();
-
 const aspect = window.innerWidth / window.innerHeight;
 const camera = new PerspectiveCamera(75, aspect, 0.1, 1000);
-
-const light = new AmbientLight(0xffffff, 3.0); // soft white light
+const light = new AmbientLight(0xffffff, 3.0);
 scene.add(light);
 scene.background = new Color('lightgray');
-
 const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.listenToKeyEvents(window); // optional
+controls.listenToKeyEvents(window);
 
 let plateau: Object3D | null;
 let plateau_mesh: Mesh;
+let INTERSECTED: Mesh | null = null;
 const clock = new Clock();
 
 function loadData() {
   new GLTFLoader()
     .setPath('assets/models/')
-    .load('vinyl_player.glb', gltfReader);
+    .load('vinyl.glb', gltfReader);
 }
 
 function gltfReader(gltf: GLTF) {
   let testModel = null;
-
   testModel = gltf.scene;
-
   if (testModel != null) {
     console.log("Model loaded: " + testModel);
     scene.add(gltf.scene);
-    plateau_mesh = scene.getObjectByName('pCylinder2_dit2_0') as Mesh;
-    let plateau_material = plateau_mesh.material as MeshStandardMaterial
-    plateau_material.wireframe = true;
+    plateau_mesh = scene.getObjectByName('platterMesh') as Mesh;
+    let plateau_material = plateau_mesh.material as MeshStandardMaterial;
+    // plateau_material.wireframe = true;
     plateau = plateau_mesh?.parent;
-
-
   } else {
-    console.log("Load FAILED.  ");
+    console.log("Load FAILED.");
   }
 }
 
 loadData();
-
-
 camera.position.z = 3;
 
-
+function onPointerMove(event: { clientX: number; clientY: number; }) {
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
 
 // Main loop
-const animation = () => {
 
-  renderer.setAnimationLoop(animation); // requestAnimationFrame() replacement, compatible with XR 
-  // plateau?.rotateY(45);
+const animation = () => {
+  renderer.setAnimationLoop(animation);
   renderer.render(scene, camera);
-};
+
+  raycaster.setFromCamera(pointer, camera);
+
+  const intersects = raycaster.intersectObjects(scene.children, true);
+
+  if (intersects.length > 0) {
+
+    let found = false;
+    for (let i = 0; i < intersects.length; i++) {
+      if (intersects[i].object instanceof Mesh) {
+        const mesh = intersects[i].object as Mesh;
+        if (mesh.material instanceof MeshStandardMaterial) {
+          if (INTERSECTED != mesh) {
+
+            if (INTERSECTED) {
+              const oldMaterial = INTERSECTED.material as MeshStandardMaterial;
+              oldMaterial.emissive.setHex(INTERSECTED.userData.currentHex || 0x000000);
+            }
+
+            INTERSECTED = mesh;
+            const material = INTERSECTED.material as MeshStandardMaterial;
+            INTERSECTED.userData.currentHex = material.emissive.getHex();
+            material.emissive.setHex(0xff0000);
+          }
+          found = true;
+          break;
+        }
+      }
+    }
+
+
+    if (!found && INTERSECTED) {
+      const material = INTERSECTED.material as MeshStandardMaterial;
+      material.emissive.setHex(INTERSECTED.userData.currentHex || 0x000000);
+      INTERSECTED = null;
+    }
+  } else {
+
+    if (INTERSECTED) {
+      const material = INTERSECTED.material as MeshStandardMaterial;
+      material.emissive.setHex(INTERSECTED.userData.currentHex || 0x000000);
+      INTERSECTED = null;
+    }
+  }
+}
 
 animation();
 
 window.addEventListener('resize', onWindowResize, false);
+window.addEventListener('pointermove', onPointerMove);
 
 function onWindowResize() {
-
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-
-
-
   renderer.setSize(window.innerWidth, window.innerHeight);
-
 }
