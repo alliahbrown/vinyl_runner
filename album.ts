f12e055b4db4e4d400dd301796a53c9dc1e379a3
vboxuser <alliah.brown@ensg.eu>
2026-03-01 00:24:30 +0000

    test1

--- album.ts
+++ album.ts
@@ -3,19 +3,19 @@ import { Album } from './vinylSelector';
 export const albums: Album[] = [
     {
         name: 'Dolling',
-        cover: '/assets/covers/dolling.jpg',
+        cover: '/public/assets/covers/dolling.jpg',
         vinylName: 'VINYL164_2',
-        audio: '/assets/audio/dolling.mp3'
+        audio: '/public/assets/audio/dolling.mp3'
     },
     {
         name: 'Lofi',
-        cover: '/assets/covers/lofi.jpg',
+        cover: '/public/assets/covers/lofi.jpg',
         vinylName: 'VINYL165_3',
-        audio: '/assets/audio/lofi.mp3'
+        audio: '/public/assets/audio/lofi.mp3'
     },
     {
         name: 'Summer',
-        cover: '/assets/covers/summer.jpg',
+        cover: '/public/assets/covers/summer.jpg',
         vinylName: 'VINYL166_4',
         audio: '/assets/audio/summer.mp3'
     },
